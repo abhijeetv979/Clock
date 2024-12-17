f530547d49ff422de6aea921a1c2acd7afced6b5
abhijeet <abhijeetv979@gmail.com>
2024-12-17 20:07:45 +0530

    add

--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
 
 // Layout Component
 function Layout({ children }) {
+
+  useEffect(() => {
+
+    const script = document.createElement("script");
+
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6330093427541658";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+
+    document.body.appendChild(script);
+
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+    catch (err) {
+
+    }
+  }, [])
+
   return (
     <div className="layout">
-      <div className="add-area">Ad Space</div>
+      <div className="add-area">
+        <ins class="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-6330093427541658"
+          data-ad-slot="7585753696"
+          data-ad-format="auto"
+          data-full-width-responsive="true">
+        </ins>
+      </div>
       <div className="main-content">{children}</div>
     </div>
   );
