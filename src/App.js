d0c59fe697807bb180f07d9351db18b20448e110
abhijeet <abhijeetv979@gmail.com>
2024-12-18 23:35:26 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import Clock from "./Clock";
 import Layout from "./Layout";
 import Stopwatch from "./Stopwatch";
 import './App.css';
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 
 function App() {
 
   const [isActive, setIsActive] = useState(false);
+  const [count, setCount] = useState(30);
 
   const isActiveHandler = (isActiveVal) => {
     setIsActive(isActiveVal);
   }
 
+  useEffect(()=>{
+    if(isActive){
+      setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+  },[isActive])
+
+  useEffect(()=>{
+    if(!isActive){
+      setCount(30);
+    }
+  },[isActive])
+
+
   return (
     <Layout>
-      <Clock isActiveHandler={isActiveHandler}/>
-      <Stopwatch isActive={isActive}/>
+      {isActive && <h1>Wait for {count} seconds!</h1>}
+      <Clock isActiveHandler={isActiveHandler} />
+      <Stopwatch isActive={isActive} />
     </Layout>
   );
 }
